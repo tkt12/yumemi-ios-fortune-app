be74eeb47ff6c18d6be2fcbeaf1a7c2c17fbe9dd
tkt12 <kinotaka1227@gmail.com>
2025-10-20 17:06:06 +0900

    add:Xcodeプロジェクトテンプレート

---
+++ YumemiFortune/YumemiFortune/YumemiFortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumemiFortuneApp.swift
+//  YumemiFortune
+//
+//  Created by tkt on 2025/10/20.
+//
+
+import SwiftUI
+
+@main
+struct YumemiFortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
